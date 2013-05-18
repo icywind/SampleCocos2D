e53284c071b0e4681e76d33718149da59ddd7876
Rickie Cheng <chengr@Rickies-MacBook-Pro.local>
2013-05-17 18:12:23 -0700

    Initial Commit

---
+++ Cocos2DSimpleGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocos2DSimpleGame
+//
+//  Created by Rickie Cheng on 5/17/13.
+//  Copyright Rickie Cheng 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
